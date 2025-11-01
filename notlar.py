f892e4c6725c6861a9bb38638d48b923388e3fad
serifcicek92 <serifcicek92@gmail.com>
2025-11-01 08:57:35 +0300

    fsaf

--- notlar.py
+++ notlar.py
@@ -3,8 +3,23 @@
 #ollama run gpt-oss:latest
 #ollama pull llama3.1:latest
 
-pip install sentence-transformers
+#python -m uvicorn main:app --reload
+#python -m uvicorn main:app --host 0.0.0.0 --port 8000 --reload
 
+#netsh advfirewall firewall add rule name="Uvicorn 8000" dir=in action=allow protocol=TCP localport=8000
+
+
+#pip install fastapi[all]
+#pip install uvicorn
+#pip install sentence-transformers
+#pip install psycopg2
+#pip install ollama
+
+#venv\Scripts\activate
+
+#wmic cpu get NumberOfLogicalProcessors
+#set OLLAMA_NUM_THREADS=all
+#ollama run llama3.1 --num-thread 12
 
 {
   "module": "Ilac",
@@ -40,6 +55,17 @@ CREATE TABLE business_rules (
     created_at TIMESTAMP DEFAULT NOW(), -- Kayıt tarihi (opsiyonel)
     embedding cube                  -- 100 boyutlu vektör
 );
+
+CREATE TABLE conversation_history
+(
+    id SERIAL PRIMARY KEY,
+    user_id character varying(64) COLLATE pg_catalog."default",
+    message_role character varying(10) COLLATE pg_catalog."default",
+    message_text text COLLATE pg_catalog."default",
+    created_at timestamp without time zone DEFAULT now()
+);
+
+
 
 🔹 Rol Tanımı
 Rol	Kullanım
